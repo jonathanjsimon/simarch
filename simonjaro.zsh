863eb408617c95aea3d02eb4b290c39a8cda7403
Jonathan Simon <simon.jonathan@gmail.com>
2022-03-02 15:13:23 -0500

    More packages and tweaks

--- simonjaro.zsh
+++ simonjaro.zsh
@@ -2,6 +2,7 @@
 
 pushd ~
 
+INSTALL_PKGS=0
 REPO_PATH="${1}"
 
 if [ -z "${REPO_PATH}" ] || ! [ -d "${REPO_PATH}" ];
@@ -43,27 +44,46 @@ EOF
 
 /usr/bin/sudo sysctl --system
 
-/usr/bin/sudo perl -p -i -e 's/^.UseSyslog/UseSyslog/g; s/^.Color/Color/g; s/^.TotalDownload/TotalDownload/g; s/^.ParallelDownloads.*/ParallelDownloads = 10/g' /etc/pacman.conf
-/usr/bin/sudo perl -p -i -e 's/^.MAKEFLAGS=.*/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
-# pamac install --no-confirm yay opendoas emacs-nox visual-studio-code-bin \
-#                             gnome-keyring brave-browser git dkms linux-headers \
-#                             figlet git bc zsh bwm-ng htop atool aria2 exa unzip \
-#                             kvantum materia-kde kvantum-theme-materia materia-gtk-theme \
-#                             gtk-engine-murrine latte-dock-git mono pnpm-bin ferdi-bin \
-#                             gnupg ulauncher ulauncher-theme-arc-dark-git dropbox nextcloud-client \
+/usr/bin/sudo wget -O /usr/share/konsole/base16-tomorrow-night.colorscheme https://raw.githubusercontent.com/cskeeters/base16-konsole/master/colorscheme/base16-tomorrow-night.colorscheme
 
+if [ ${INSTALL_PKGS} -gt 0 ];
+then
+    /usr/bin/sudo perl -p -i -e 's/^.UseSyslog/UseSyslog/g; s/^.Color/Color/g; s/^.TotalDownload/TotalDownload/g; s/^.ParallelDownloads.*/ParallelDownloads = 10/g' /etc/pacman.conf
+    /usr/bin/sudo perl -p -i -e 's/^.MAKEFLAGS=.*/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
+    # remove some things
+    pamac remove kate
 
-# pamac install --no-confirm mono-git wine-valve proton
+    # install a bunch
+    pamac install --no-confirm yay opendoas emacs-nox visual-studio-code-bin \
+                                gnome-keyring brave-browser git dkms linux-headers \
+                                figlet git bc zsh bwm-ng htop atool aria2 exa unzip \
+                                kvantum materia-kde kvantum-theme-materia materia-gtk-theme \
+                                gtk-engine-murrine latte-dock-git mono pnpm-bin ferdi-bin \
+                                gnupg ulauncher ulauncher-theme-arc-dark-git dropbox nextcloud-client \
+                                papirus-icon-theme spotify teams slack-desktop mailspring \
+                                virtualbox virtualbox-guest-iso virtualbox-ext-oracle \
+                                rustup gdb lldb kgpg kwrite plasma5-applets-virtual-desktop-bar-git \
+                                discover baobab dotnet-host dotnet-runtime dotnet-runtime-3.1 dotnet-sdk \
+                                dotnet-sdk-3.1 dotnet-targeting-pack dotnet-targeting-pack-3.1 aspnet-runtime \
+                                aspnet-runtime-3.1 aspnet-targeting-pack aspnet-targeting-pack-3.1
 
+    # installing this separately because it seems to no longer well and I wanted to be able to comment it out
+    pamac install --no-confirm superpaper
 
-#yay -S emacs-nox visual-studio-code-bin gnome-keyring brave-browser git
+    # get that rust
+    rustup toolchain install stable
+    rustup target add i686-unknown-linux-gnu
 
-#exit 0
+    # install some AUR things that take a while to compile
+    # mono-git build-depends on mono which is installed in the prior steps
+    pamac install --no-confirm mono-git wine-valve proton
 
+    # set icons this way cuz I can
+    /usr/lib/plasma-changeicons Papirus-Dark
+fi
 
 LAST_SNAPSHOT=`borg list --short --last 1 "${REPO_PATH}"`
 echo "${LAST_SNAPSHOT}"
-# borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/{Desktop,Documents,Music,techsupport,Videos,VirtualBox\ VMs,Downloads,Development,Dropbox,.ssh,.gnupg,.gitconfig,/.config/BraveSoftware/Brave-Browser,.config/Ferdi}
+# borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/{Desktop,Documents,Music,techsupport,Videos,VirtualBox\ VMs,Downloads,Development,Dropbox,.ssh,.gnupg,.gitconfig,/.config/BraveSoftware/Brave-Browser,.config/Ferdi,.config/latte,.config/superpaper}
 
-# borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/{techsupport,.ssh}
-# borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/.config/Ferdi
+borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/.config/latte
