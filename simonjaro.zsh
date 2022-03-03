6c0c9c435dd8813faec3aac6b6830d840c491a35
Jonathan Simon <simon.jonathan@gmail.com>
2022-03-03 00:36:28 -0500

    Some organization

--- simonjaro.zsh
+++ simonjaro.zsh
@@ -28,11 +28,15 @@ then
     exit 3
 fi
 
-cat << EOF | /usr/bin/sudo tee /etc/udev/rules.d/81-wifi-powersave.rules
+cat << 'EOF' | /usr/bin/sudo tee /etc/udev/rules.d/81-wifi-powersave.rules
 # never power save wifi, the chip will disconnect from the network randomly on 5GHz
 
 ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/bin/iw dev $name set power_save off"
 EOF
+
+for dev in /sys/class/net/*; do
+    [ -e "$dev"/wireless ] && /usr/bin/sudo /usr/bin/iw dev ${dev##*/} set power_save off
+done
 
 cat << EOF | /usr/bin/sudo tee /etc/doas.conf
 permit persist :wheel
@@ -50,25 +54,53 @@ if [ ${INSTALL_PKGS} -gt 0 ];
 then
     /usr/bin/sudo perl -p -i -e 's/^.UseSyslog/UseSyslog/g; s/^.Color/Color/g; s/^.TotalDownload/TotalDownload/g; s/^.ParallelDownloads.*/ParallelDownloads = 10/g' /etc/pacman.conf
     /usr/bin/sudo perl -p -i -e 's/^.MAKEFLAGS=.*/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
+
     # remove some things
     pamac remove kate
+
+    # some import starters
+    pamac install --no-confirm caffeine-ng gnupg kgpg kwrite 1password opendoas emacs-nox gnome-keyring brave-browser git figlet bc dkms linux-headers zsh htop bwm-ng aria2 exa unzip
+
+    # remove pulse
     sudo pacman -Rdd pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf
+    # install pipewire
     sudo pacman -S manjaro-pipewire gst-plugin-pipewire plasma-pa pipewire-jack easyeffects pipewire-x11-bell realtime-privileges xdg-desktop-portal-kde
 
-    # install a bunch
-    pamac install --no-confirm yay opendoas emacs-nox visual-studio-code-bin \
-                                gnome-keyring brave-browser git dkms linux-headers \
-                                figlet git bc zsh bwm-ng htop atool aria2 exa unzip \
-                                kvantum materia-kde kvantum-theme-materia materia-gtk-theme \
-                                gtk-engine-murrine latte-dock-git mono pnpm-bin ferdi-bin \
-                                gnupg ulauncher ulauncher-theme-arc-dark-git dropbox nextcloud-client \
-                                papirus-icon-theme spotify teams slack-desktop mailspring \
-                                virtualbox virtualbox-guest-iso virtualbox-ext-oracle \
-                                rustup gdb lldb kgpg kwrite plasma5-applets-virtual-desktop-bar-git \
-                                discover baobab dotnet-host dotnet-runtime dotnet-runtime-3.1 dotnet-sdk \
+    # theme stuff
+    pamac install --no-confirm kvantum materia-kde kvantum-theme-materia materia-gtk-theme gtk-engine-murrine papirus-icon-theme plasma5-applets-virtual-desktop-bar-git
+
+    # dock and ulauncher stuff
+    pamac install --no-confirm latte-dock-git ulauncher ulauncher-theme-arc-dark-git
+
+    # cloud stuff
+    pamac install --no-confirm dropbox nextcloud-client
+
+    # spotify AUR installer fails sometimes
+    pamac install --no-confirm spotify
+
+    # chat and email
+    pamac install --no-confirm teams slack-desktop mailspring ferdi-bin pnpm-bin
+
+    # archive tool
+    pamac install --no-confirm atool
+
+    # code
+    pamac install --no-confirm visual-studio-code-bin
+
+    # VMs
+    pamac install --no-confirm virtualbox virtualbox-guest-iso virtualbox-ext-oracle
+
+    # rust
+    pamac install --no-confirm rustup gdb lldb
+
+    # misc
+    pamac install --no-confirm discover baobab obsidian npm
+
+    # dotnet core
+    pamac install --no-confirm dotnet-host dotnet-runtime dotnet-runtime-3.1 dotnet-sdk \
                                 dotnet-sdk-3.1 dotnet-targeting-pack dotnet-targeting-pack-3.1 aspnet-runtime \
-                                aspnet-runtime-3.1 aspnet-targeting-pack aspnet-targeting-pack-3.1 \
-                                obsidian npm
+                                aspnet-runtime-3.1 aspnet-targeting-pack aspnet-targeting-pack-3.1
+
 
     # installing this separately because it seems to no longer well and I wanted to be able to comment it out
     pamac install --no-confirm superpaper
@@ -80,6 +112,8 @@ then
     # install some npm stuff
     sudo npm i -g html-minifier uglify-js uglifycss sass
 
+    # mono, gotta check if mono-git is already installed so we don't do this twice
+    pamac install --no-confirm mono
     # install some AUR things that take a while to compile
     # mono-git build-depends on mono which is installed in the prior steps
     pamac install --no-confirm mono-git wine-valve proton
