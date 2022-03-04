05aadae698a3f373003062de96990b9194bd8f16
Jonathan Simon <simon.jonathan@gmail.com>
2022-03-03 20:18:40 -0500

    Structural refactor

--- simonjaro.zsh
+++ simonjaro.zsh
@@ -1,63 +1,29 @@
 #!/usr/bin/zsh
 
-pushd ~
-
 INSTALL_PKGS=0
-REPO_PATH="${1}"
+BORG_RESTORE=0
+REPO_PATH=""
+PASSPHRASE=""
 
-if [ -z "${REPO_PATH}" ] || ! [ -d "${REPO_PATH}" ];
-then
-    echo "Invalid repo path"
-    exit 1
-fi
+function binary_exists()
+{
 
-PASSPHRASE=''
-read "PASSPHRASE?Repo passphrase (${REPO_PATH}): "
+    if [ -z "${1}" ]; then
+        return 1
+    fi
 
-if [ -z "${PASSPHRASE}" ];
-then
-    echo "Must supply repo passphrase"
-    exit 2
-fi
+    if (( $+commands[${1}] ))
+    then
+        return 0
+    fi
 
-export BORG_PASSPHRASE="${PASSPHRASE}"
-borg info "${REPO_PATH}" &> /dev/null
-if [ $? -gt 0 ] ;
-then
-    echo "Passhrase incorrect"
-    exit 3
-fi
+    return 1
+}
 
-cat << 'EOF' | /usr/bin/sudo tee /etc/udev/rules.d/81-wifi-powersave.rules
-# never power save wifi, the chip will disconnect from the network randomly on 5GHz
-
-ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/bin/iw dev $name set power_save off"
-EOF
-
-for dev in /sys/class/net/*; do
-    [ -e "$dev"/wireless ] && /usr/bin/sudo /usr/bin/iw dev ${dev##*/} set power_save off
-done
-
-cat << EOF | /usr/bin/sudo tee /etc/doas.conf
-permit persist :wheel
-EOF
-
-cat << EOF | /usr/bin/sudo tee /etc/sysctl.d/99-max-watchers.conf
-fs.inotify.max_user_watchers = 1000000
-EOF
-
-/usr/bin/sudo sysctl --system
-
-/usr/bin/sudo wget -O /usr/share/konsole/base16-tomorrow-night.colorscheme https://raw.githubusercontent.com/cskeeters/base16-konsole/master/colorscheme/base16-tomorrow-night.colorscheme
-
-if [ ${INSTALL_PKGS} -gt 0 ];
-then
-    /usr/bin/sudo perl -p -i -e 's/^.UseSyslog/UseSyslog/g; s/^.Color/Color/g; s/^.TotalDownload/TotalDownload/g; s/^.ParallelDownloads.*/ParallelDownloads = 10/g;' /etc/pacman.conf
-    /usr/bin/sudo perl -p -i -e 's/^.EnableAUR/EnableAUR/g; s/^.EnableSnap/EnableSnap/g; s/^.EnableFlatpak/EnableFlatpak/g; s/^.CheckFlatpakUpdates/CheckFlatpakUpdates/g;' /etc/pamac.conf
-    /usr/bin/sudo perl -p -i -e 's/^.MAKEFLAGS=.*/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
-
+function install_packages()
+{
     # some more AUR helpers for completion
-    pamac install --no-confirm yay trizen paru pacaur
+    pamac install --no-confirm yay paru asp devtools python-dulwich python-fastimport python-gpgme python-paramiko bat
 
     # remove pulse
     /usr/bin/sudo pacman -Rdd pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf
@@ -125,18 +91,125 @@ then
     # install some npm stuff
     sudo npm i -g html-minifier uglify-js uglifycss sass
 
-    # mono, gotta check if mono-git is already installed so we don't do this twice
-    pamac install --no-confirm mono
+    # mono-git build-depends on mono so we have to install it first and then replace with mono-git
+    if ! binary_exists mono;
+    then
+        pamac install --no-confirm mono
+        pamac install --no-confirm mono-git mono-msbuild
+    fi
+
     # install some AUR things that take a while to compile
-    # mono-git build-depends on mono which is installed in the prior steps
-    pamac install --no-confirm mono-git wine-valve proton
+    pamac install --no-confirm wine-valve proton
 
     # set icons this way cuz I can
     /usr/lib/plasma-changeicons Papirus-Dark
-fi
+}
 
-LAST_SNAPSHOT=`borg list --short --last 1 "${REPO_PATH}"`
-echo "${LAST_SNAPSHOT}"
-borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/{Desktop,Documents,Music,techsupport,Videos,VirtualBox\ VMs,Downloads,Development,Dropbox,.ssh,.gnupg,.gitconfig,/.config/BraveSoftware/Brave-Browser,.config/Ferdi,.config/superpaper,.config/obsidian}
+function main()
+{
+    pushd ~
 
-# borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/.config/obsidian
+    # Configure pamac and pacman now so we can install dependencies
+    /usr/bin/sudo perl -p -i -e 's/^.UseSyslog/UseSyslog/g; s/^.Color/Color/g; s/^.TotalDownload/TotalDownload/g; s/^.ParallelDownloads.*/ParallelDownloads = 10/g;' /etc/pacman.conf
+    /usr/bin/sudo perl -p -i -e 's/^.EnableAUR/EnableAUR/g; s/^.EnableSnap/EnableSnap/g; s/^.EnableFlatpak/EnableFlatpak/g; s/^.CheckFlatpakUpdates/CheckFlatpakUpdates/g;' /etc/pamac.conf
+    /usr/bin/sudo perl -p -i -e 's/^.MAKEFLAGS=.*/MAKEFLAGS="-j8"/g' /etc/makepkg.conf
+
+    if [ ${BORG_RESTORE} -gt 0 ]
+    then
+        if ! binary_exists borg;
+        then
+            echo "borg must be installed"
+            INSTALL_BORG=''
+            while [ "${INSTALL_BORG:l}" != 'y' ] && [ "${INSTALL_BORG:l}" != 'n' ];
+            do
+                read "INSTALL_BORG?Install borg [y/N]? "
+            done
+
+            if [ "${INSTALL_BORG:l}" == 'y' ];
+            then
+                pamac install --no-confirm borg
+            fi
+        fi
+
+        if binary_exists borg;
+        then
+            while [ -z "${REPO_PATH}" ] || ! [ -d "${REPO_PATH}" ];
+            do
+                read "REPO_PATH?Borg repo path? "
+            done
+
+            while [ -z "${PASSPHRASE}" ];
+            do
+                read "PASSPHRASE?Repo passphrase (${REPO_PATH}): "
+                if [ -n "${PASSPHRASE}" ];
+                then
+                    export BORG_PASSPHRASE="${PASSPHRASE}"
+                    borg info "${REPO_PATH}" &> /dev/null
+                    if [ $? -gt 0 ] ;
+                    then
+                        echo "Passphrase incorrect"
+                        PASSPHRASE=""
+                    fi
+                fi
+            done
+        else
+            echo "borg still not installed, ignoring borg restore request"
+        fi
+    fi
+
+    cat << 'EOF' | /usr/bin/sudo tee /etc/udev/rules.d/81-wifi-powersave.rules
+# never power save wifi, the chip will disconnect from the network randomly on 5GHz
+
+ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="/usr/bin/iw dev $name set power_save off"
+EOF
+
+    for dev in /sys/class/net/*; do
+        [ -e "$dev"/wireless ] && /usr/bin/sudo /usr/bin/iw dev ${dev##*/} set power_save off
+    done
+
+    cat << EOF | /usr/bin/sudo tee /etc/doas.conf
+permit persist :wheel
+EOF
+
+    cat << EOF | /usr/bin/sudo tee /etc/sysctl.d/99-max-watchers.conf
+fs.inotify.max_user_watchers = 1000000
+EOF
+
+    /usr/bin/sudo sysctl --system
+
+    /usr/bin/sudo wget -O /usr/share/konsole/base16-tomorrow-night.colorscheme https://raw.githubusercontent.com/cskeeters/base16-konsole/master/colorscheme/base16-tomorrow-night.colorscheme
+
+    if [ ${INSTALL_PKGS} -gt 0 ];
+    then
+        install_packages
+    fi
+
+    if [ ${BORG_RESTORE} -gt 0 ];
+    then
+        LAST_SNAPSHOT=`borg list --short --last 1 "${REPO_PATH}"`
+        echo "${LAST_SNAPSHOT}"
+        borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/{Desktop,Documents,Music,techsupport,Videos,VirtualBox\ VMs,Downloads,Development,Dropbox,.ssh,.gnupg,.gitconfig,/.config/BraveSoftware/Brave-Browser,.config/Ferdi,.config/superpaper,.config/obsidian}
+        # borg --progress extract --strip-components 2 "${REPO_PATH}::${LAST_SNAPSHOT}" home/${USER}/.config/obsidian
+    fi
+}
+
+for arg in "$@"
+do
+    case "$arg" in
+        --borg)
+            BORG_RESTORE=1
+            exit 0
+        ;;
+        --borg=*)
+            BORG_RESTORE=1
+            REPO_PATH="${arg#--borg=}"
+            exit 0
+        ;;
+        --pkgs)
+            INSTALL_PKGS=1
+            exit 0
+        ;;
+    esac
+done
+
+main
