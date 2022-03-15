47aeeb2dafb0d66f1b1001875b0c6ba553217411
Jonathan Simon <jonathan@autonomic-controls.com>
2022-03-15 12:17:33 -0400

    Some tweaks for VMs and XFCE

--- simonjaro.zsh
+++ simonjaro.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
 
 INSTALL_PKGS=0
+MIN_PKGS=0
 BORG_RESTORE=0
 REPO_PATH=""
 PASSPHRASE=""
@@ -22,33 +23,63 @@ function binary_exists()
 
 function install_packages()
 {
+    IS_VM=0
+    if [ "$(sudo dmidecode -s system-manufacturer)" = "innotek GmbH" ];
+    then
+        IS_VM=1
+    fi
+
     yay_options=("--useask" "--sudoloop" "--nocleanmenu" "--nodiffmenu" "--noconfirm")
     # ${yay_options[@]}
 
     # get that rust, needed for some other packages + yay
-    /usr/bin/sudo pamac install  --no-confirm rustup gdb lldb yay base-devel
+    /usr/bin/sudo pamac install  --no-confirm rustup gdb lldb yay base-devel python python-pip
     rustup toolchain install stable
     rustup target add i686-unknown-linux-gnu
 
     # theme stuff
-    yes | yay ${yay_options[@]} -S kvantum materia-kde kvantum-theme-materia materia-gtk-theme gtk-engine-murrine papirus-icon-theme
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        yes | yay ${yay_options[@]} -S kvantum materia-kde kvantum-theme-materia materia-gtk-theme gtk-engine-murrine papirus-icon-theme
 
-    # set the theme
-    /usr/bin/lookandfeeltool -a com.github.varlesh.materia-dark
-    kvantummanager --set MateriaDark
+        # set the theme
+        /usr/bin/lookandfeeltool -a com.github.varlesh.materia-dark
+        kvantummanager --set MateriaDark
+        /usr/bin/sudo wget -O /usr/share/konsole/base16-tomorrow-night.colorscheme https://raw.githubusercontent.com/cskeeters/base16-konsole/master/colorscheme/base16-tomorrow-night.colorscheme
+    fi
 
-    # remove pulse, kate, etc
-    yes | yay ${yay_options[@]} -Rdd pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf manjaro-pulse jack2 kate
-    # install pipewire
-    yes | yay ${yay_options[@]} -S manjaro-pipewire gst-plugin-pipewire plasma-pa pipewire-jack easyeffects pipewire-x11-bell realtime-privileges xdg-desktop-portal-kde
+    if [ "$XDG_CURRENT_DESKTOP" = "XFCE" ];
+    then
+        mkdir -p ~/.local/share/xfce4/terminal/colorschemes/
+        wget -O ~/.local/share/xfce4/terminal/colorschemes/base16-tomorrow-night.16.theme https://raw.githubusercontent.com/afq984/base16-xfce4-terminal/master/colorschemes/base16-tomorrow-night.16.theme
+        wget -O ~/.local/share/xfce4/terminal/colorschemes/base16-tomorrow-night.theme https://raw.githubusercontent.com/afq984/base16-xfce4-terminal/master/colorschemes/base16-tomorrow-night.theme
+    fi
+
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # remove pulse, kate, etc
+        yes | yay ${yay_options[@]} -Rdd pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-ctl pulseaudio-zeroconf manjaro-pulse jack2 kate
+        # install pipewire
+        yes | yay ${yay_options[@]} -S manjaro-pipewire gst-plugin-pipewire plasma-pa pipewire-jack easyeffects pipewire-x11-bell realtime-privileges xdg-desktop-portal-kde
+
+        yes | yay ${yay_options[@]} -S kgpg kwrite 1password
+    fi
 
     # some import starters
-    yes | yay ${yay_options[@]} -S caffeine-ng gnupg kgpg kwrite 1password opendoas emacs-nox gnome-keyring brave-browser git figlet bc zsh htop bwm-ng aria2 exa unzip
+    yes | yay ${yay_options[@]} -S caffeine-ng gnupg opendoas emacs-nox gnome-keyring brave-browser git figlet bc zsh htop bwm-ng aria2 exa unzip
+
 
     # virtualbox + linux kernel headers - DKMS should update after installation in next step
-    target_kernel_headers=$(for f in `mhwd-kernel -li | awk 'NR>2 {print $2}'`; do pkgnamebase=`basename "${f%.*}"`; echo "${pkgnamebase}-headers ${pkgnamebase}-virtualbox-host-modules"; done | paste -sd ' ')
-    target_kernel_headers_array=("${(@s/ /)target_kernel_headers}")
-    yes | yay ${yay_options[@]} -S virtualbox virtualbox-guest-iso virtualbox-ext-oracle virtualbox-host-dkms linux-headers dkms ${target_kernel_headers_array[@]}
+    if [ $IS_VM -eq 1 ];
+    then
+        target_kernel_headers=$(for f in `mhwd-kernel -li | awk 'NR>2 {print $2}'`; do pkgnamebase=`basename "${f%.*}"`; echo "${pkgnamebase}-headers"; done | paste -sd ' ')
+        target_kernel_headers_array=("${(@s/ /)target_kernel_headers}")
+        yes | yay ${yay_options[@]} -S virtualbox-guest-utils linux-headers dkms ${target_kernel_headers_array[@]}
+    else
+        target_kernel_headers=$(for f in `mhwd-kernel -li | awk 'NR>2 {print $2}'`; do pkgnamebase=`basename "${f%.*}"`; echo "${pkgnamebase}-headers ${pkgnamebase}-virtualbox-host-modules"; done | paste -sd ' ')
+        target_kernel_headers_array=("${(@s/ /)target_kernel_headers}")
+        yes | yay ${yay_options[@]} -S virtualbox virtualbox-guest-iso virtualbox-ext-oracle virtualbox-host-dkms linux-headers dkms ${target_kernel_headers_array[@]}
+    fi
 
     # for some reason, linux414 headers get installed
     yes | yay ${yay_options[@]} -Rdd linux414-headers
@@ -57,40 +88,62 @@ function install_packages()
     # gnome-keyring
     yes | yay ${yay_options[@]} -S libgnome-keyring
 
-    # dock and ulauncher stuff
-    yes | yay ${yay_options[@]} -S latte-dock-git ulauncher ulauncher-theme-arc-dark-git plasma5-applets-virtual-desktop-bar-git
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # dock and ulauncher stuff
+        yes | yay ${yay_options[@]} -S latte-dock-git ulauncher ulauncher-theme-arc-dark-git plasma5-applets-virtual-desktop-bar-git
+        systemctl --user enable --now ulauncher.service
+    fi
 
-    systemctl --user enable --now ulauncher.service
+    if [ $IS_VM -ne 1 ] && [ ${MIN_PKGS} -eq 0 ];
+    then
+        # cloud stuff
+        yes | yay ${yay_options[@]} -S dropbox nextcloud-client
 
-    # cloud stuff
-    yes | yay ${yay_options[@]} -S dropbox nextcloud-client
+        # spotify AUR installer fails sometimes
+        yes | yay ${yay_options[@]} -S spotify
 
-    # spotify AUR installer fails sometimes
-    yes | yay ${yay_options[@]} -S spotify
+        # chat and email
+        yes | yay ${yay_options[@]} -S teams slack-desktop mailspring ferdi-nightly-bin pnpm-bin zoom
 
-    # chat and email
-    yes | yay ${yay_options[@]} -S teams slack-desktop mailspring ferdi-nightly-bin pnpm-bin zoom
-
-    # borg + vorta
-    yes | yay ${yay_options[@]} -S borg vorta
+        # borg + vorta
+        yes | yay ${yay_options[@]} -S borg vorta
+    fi
 
     # archive tool
     yes | yay ${yay_options[@]} -S atool
 
     # code
-    yes | yay ${yay_options[@]} -S visual-studio-code-bin gitkraken
+    yes | yay ${yay_options[@]} -S visual-studio-code-bin
+
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # gitkraken
+        yes | yay ${yay_options[@]} -S gitkraken
+    fi
+
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # misc
+        yes | yay ${yay_options[@]} -S obsidian todoist-appimage freecad
+    fi
+
+    # if KDE
+    # yes | yay ${yay_options[@]} -S discover
 
     # misc
-    yes | yay ${yay_options[@]} -S discover baobab obsidian npm todoist-appimage freecad
+    yes | yay ${yay_options[@]} -S baobab npm
 
     # dotnet core
     yes | yay ${yay_options[@]} -S dotnet-host dotnet-runtime dotnet-runtime-3.1 dotnet-sdk \
                                 dotnet-sdk-3.1 dotnet-targeting-pack dotnet-targeting-pack-3.1 aspnet-runtime \
                                 aspnet-runtime-3.1 aspnet-targeting-pack aspnet-targeting-pack-3.1
 
-
-    # installing this separately because it seems to no longer well and I wanted to be able to comment it out
-    yes | yay ${yay_options[@]} -S superpaper
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # installing this separately because it seems to no longer well and I wanted to be able to comment it out
+        yes | yay ${yay_options[@]} -S superpaper
+    fi
 
     # network tools
     yes | yay ${yay_options[@]} -S speedtest-cli speedtest++ protonvpn-gui
@@ -108,10 +161,17 @@ function install_packages()
     then
         yes | yay ${yay_options[@]} -S automake autoconf
         yes | yay ${yay_options[@]} -S mono
-        yes | yay ${yay_options[@]} -S mono-git mono-msbuild
+        if [ ${MIN_PKGS} -eq 0 ];
+        then
+            yes | yay ${yay_options[@]} -S mono-git mono-msbuild
+        fi
     fi
-    # yes | yay ${yay_options[@]} -S mingw-w64-freetype2
-    yes | yay ${yay_options[@]} -S wine-valve proton
+
+    if [ ${MIN_PKGS} -eq 0 ];
+    then
+        # yes | yay ${yay_options[@]} -S mingw-w64-freetype2
+        yes | yay ${yay_options[@]} -S wine-valve proton
+    fi
 }
 
 function main()
@@ -223,8 +283,6 @@ EOF
 
     /usr/bin/sudo sysctl --system
 
-    /usr/bin/sudo wget -O /usr/share/konsole/base16-tomorrow-night.colorscheme https://raw.githubusercontent.com/cskeeters/base16-konsole/master/colorscheme/base16-tomorrow-night.colorscheme
-
     if [ ${INSTALL_PKGS} -gt 0 ];
     then
         install_packages
@@ -251,6 +309,10 @@ do
         ;;
         --pkgs)
             INSTALL_PKGS=1
+        ;;
+        --minpkgs)
+            INSTALL_PKGS=1
+            MIN_PKGS=1
         ;;
     esac
 done
