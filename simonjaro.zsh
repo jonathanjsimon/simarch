1969d8a6edf1110ae2df0028e60941d9c8c7254b
Jonathan Simon <simon.jonathan@gmail.com>
2022-12-03 23:27:16 -0500

    Fix bbr kmod name

--- simonjaro.zsh
+++ simonjaro.zsh
@@ -368,7 +368,7 @@ net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
 
-modprobe bbr
+modprobe tcp_bbr
 sysctl -p /etc/sysctl.d/bbr.conf
 sysctl net.ipv4.tcp_available_congestion_control
 sysctl net.ipv4.tcp_congestion_control
