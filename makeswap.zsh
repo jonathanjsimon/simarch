0d901f88b501ce9769e237c24d7b3dcad231f809
Jonathan Simon <simon.jonathan@gmail.com>
2023-10-10 13:31:12 -0400

    Initial swapfile script w/ dracut integration

---
+++ makeswap.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/zsh
+
+${SWAPFILE}="/swapfile"
+
+if [ -f ${SWAPFILE} ];
+then
+    sudo swapoff ${SWAPFILE}
+    sudo rm -fv ${SWAPFILE}
+fi
+
+sudo dd status=progress if=/dev/zero of=${SWAPFILE} bs=1M count=$((32 * 1024)) && \
+    sudo chmod 600 /swapfile && \
+    sudo mkswap -U clear /swapfile && \
+    sudo swapon /swapfile && \
+    sudo findmnt -no UUID -T /swapfile && \
+    sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}' && \
+    cat <<-EOF | sudo tee /etc/dracut.conf.d/resume.conf
+add_dracutmodules+=" resume "
+EOF
